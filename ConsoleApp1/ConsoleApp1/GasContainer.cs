79ad4aca338950c78430801236ce6d382e757282
s28691 <s28691@pjwstk.edu.pl>
2024-03-25 21:06:42 +0100

    GasContainer class

---
+++ ConsoleApp1/ConsoleApp1/GasContainer.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1;
+
+public class GasContainer : Container, IHazardNotifier
+{
+    private double pressure;
+    public GasContainer(double mass, double height, double contMass, double depth, double maxMass, double pressure)
+        :base(mass, height, contMass, depth, maxMass)
+    {
+        this.pressure = pressure;
+        string SN = GetSN() + "G-" + GetId();
+        setSN(SN);
+    }
+    public void Unload()
+    {
+        double newMass = GetMass() * 0.05;
+        ChangeMass(newMass);
+    }
+ 
+    public void Notify()
+    {
+        Console.WriteLine("Dangerous alert" + GetSN());
+    }
+
+}
